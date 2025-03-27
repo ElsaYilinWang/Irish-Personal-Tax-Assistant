226e91b808de6daf778d05d3f34a24b411a4e2c9
elsawang <elsa.yilin.w39@gmail.com>
2025-03-27 19:58:18 +0000

    frontend setup

---
+++ src/components/TaxForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TaxForm() {
+  return (
+    <div>
+      <h1>Tax Form</h1>
+      <p>Fill out your tax information here.</p>
+    </div>
+  );
+}
+
+export default TaxForm;
